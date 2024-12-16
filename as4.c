232e53f9cbe86e65297f9ed1ecce07310134f76d
Mirusna <mirusnasaravanan@gmail.com>
2024-12-16 19:56:27 +0530

    Update as4.c

--- as4.c
+++ as4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-
 int numJewelsInStones(char *jewels, char *stones) {
     int count = 0;
     for (int i = 0; stones[i] != '\0'; i++) {
@@ -11,16 +10,13 @@ int numJewelsInStones(char *jewels, char *stones) {
     }
     return count;
 }
-
 int main() {
     char jewels[100], stones[100];
     printf("Enter the jewels string: ");
     scanf("%s", jewels);
     printf("Enter the stones string: ");
     scanf("%s", stones);
-
     int count = numJewelsInStones(jewels, stones);
     printf("Number of jewels in stones: %d\n", count);
-
     return 0;
 }
